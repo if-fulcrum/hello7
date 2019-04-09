833bc710f1f7a023186c8ecedd355b122d56401d
Kenn Herman <kenn@ifsight.com>
2019-04-09 10:55:36 -0700

    new fulcrum magic

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -585,8 +585,4 @@ $conf['404_fast_html'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
  */
 # $conf['theme_debug'] = TRUE;
 
-if ($ff = '/fulcrum/fulcrum.php' AND file_exists($ff) AND (include $ff) AND isset($_FULCRUM)) {
-  fulcrum_cfg('pre', $_FULCRUM['conf']);
-} else if ($ff = "{$_SERVER['HOME']}/fulcrum/php/fulcrum.php" AND file_exists($ff) AND (include $ff) AND isset($_FULCRUM)) {
-  fulcrum_cfg('pre', $_FULCRUM['conf']);
-}
+fulcrum_config('pre');
